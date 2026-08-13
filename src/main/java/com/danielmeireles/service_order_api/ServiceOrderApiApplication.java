099c31766c767ac1673ca078c20e39a0f4544fd1
DanielMeirelesSi <daniel.meireles.pro@gmail.com>
2026-08-13 13:49:33 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/danielmeireles/service_order_api/ServiceOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.danielmeireles.service_order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielmeireles/service_order_api/ServiceOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielmeireles.service_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
